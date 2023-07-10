ce9164eed152072b4621ed3b9c33ad3b5c9d7a70
Cowboy Ho <dreamran43@gmail.com>
2023-07-11 00:58:05 +0800

    Reproduce ths issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // webpack: config => {
+  //   let modularizeImports = null
+  //   config.module.rules.some(rule => (
+  //     rule.oneOf?.some(oneOf => {
+  //       modularizeImports = oneOf?.use?.options?.nextConfig?.modularizeImports
+  //       return modularizeImports
+  //     })
+  //   ))
+  //   if (modularizeImports?.['react-bootstrap'])
+  //     delete modularizeImports['react-bootstrap']
+  //   return config
+  // }
 }
 
 module.exports = nextConfig
